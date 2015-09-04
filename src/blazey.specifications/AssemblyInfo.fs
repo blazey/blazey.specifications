ee7b7a0fcb02b4f8276156431043f1b900ff5b9c
Ed Blackburn <ed@ejb.name>
2015-09-04 14:20:23 +0100

    ProjectSaffhold structure

---
+++ src/blazey.specifications/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("blazey.specifications")>]
+[<assembly: AssemblyProductAttribute("blazey.specifications")>]
+[<assembly: AssemblyDescriptionAttribute("mspec compatible xunit.net runner")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
